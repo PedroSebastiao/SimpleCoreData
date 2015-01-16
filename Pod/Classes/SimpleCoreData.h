fa562dbd296b507f6dee22ac5c43a35bed50a0f4
Pedro Sebastião <pedro@sebastiao.eu>
2015-01-16 01:03:43 +0000

    added header with necessary lib imports

---
+++ Pod/Classes/SimpleCoreData.h
@@ -0,0 +1,11 @@
+//
+//  SimpleCoreData.h
+//
+//
+//  Created by Pedro Sebastião on 16/01/15.
+//
+
+#import "PSCoreDataStackManager.h"
+#import "NSFetchRequest+Extension.h"
+#import "NSManagedObject+Extension.h"
+#import "NSManagedObjectContext+Extension.h"
